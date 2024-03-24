d0b86db26112bd41b9833198d7fe4709fa692518
sue <dldustn2040@gmail.com>
2024-03-25 02:42:55 +0900

    2024-03-25

--- python/2506_점수계산.py
+++ python/2506_점수계산.py
@@ -1,9 +1,13 @@
 n = int(input())
-score = list(map(int, input()))
-total_score = 0
+score = list(map(int, input().split()))
+cnt, total_score = 0, 0
 
-for i in len(score) :
+for i in range(n) :
+  if score[i] == 0 :
+    cnt = 0
+    continue
   if score[i] == 1 :
-    total_score += 1
-    if i > 0 and score[i-1] == 1 :
-      total_score += 1
+    cnt += 1
+    total_score += cnt
+    
+print(total_score)
