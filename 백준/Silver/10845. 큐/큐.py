360497c8367a2b91494e518da55760d2d3803e11
이연수 <dldustn2040@gmail.com>
2024-05-07 17:14:59 +0900

    [Silver IV] Title: 큐, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.py
+++ 백준/Silver/10845. 큐/큐.py
@@ -3,49 +3,30 @@ import sys
 n = int(sys.stdin.readline())
 queue = []
 
-def push(queue, x) :
-    queue.append(x)
-
-def pop(queue) :
-    if len(queue) > 0 :
-        print(queue[0])
-        queue.__delitem__(0)
-    else:
-        print(-1)
-
-def size(queue) :
-    print(len(queue))
-
-def empty(queue) :
-    if len(queue) > 0 :
-        print(0)
-    else :
-        print(1)
-
-def front(queue) :
-    if len(queue) > 0:
-        print(queue[0])
-    else :
-        print(-1)
-
-def back(queue) :
-    if len(queue) > 0:
-        print(queue[-1])
-    else :
-        print(-1)
-
-
 for _ in range(n) :
     order = sys.stdin.readline().split()
-    if 'push' in order:
-        push(queue, order[1])
+    if order[0] == 'push':
+        queue.append(order[1])
     elif order[0] == 'pop':
-        pop(queue)
+        if len(queue) > 0 :
+            print(queue[0])
+            queue.__delitem__(0)
+        else:
+            print(-1)
     elif order[0] == 'size':
-        size(queue)
+        print(len(queue))
     elif order[0] == 'empty':
-        empty(queue)
+        if len(queue) > 0 :
+            print(0)
+        else :
+            print(1)
     elif order[0] == 'front':
-        front(queue)
+        if len(queue) > 0:
+            print(queue[0])
+        else :
+            print(-1)
     elif order[0] == 'back':
-        back(queue)
+        if len(queue) > 0:
+            print(queue[-1])
+        else :
+            print(-1)
