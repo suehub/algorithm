5088e7c620aac820c1c2c44c86fecced703bde03
이연수 <dldustn2040@gmail.com>
2024-05-10 08:52:30 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 64 ms, Memory: 34008 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -5,15 +5,8 @@ n, k = map(int, sys.stdin.readline().split())
 q = deque([x for x in range(1, n+1)])
 result = []
 
-while len(q) > 0:   
-    item = q[(k-1) % len(q)]    # 삭제할 값. q크기 < k 일 때 나머지 연산 필요
-    result.append(str(item))
-
-    for _ in range(q.index(item)):  # 제거할 원소의 이전 원소들 맨 뒤로 이동
-        temp = q[0]
-        q.popleft()
-        q.append(temp)
-
-    q.remove(item)
+while q:
+    q.rotate(-k + 1)  # k번째 요소를 맨 앞으로 옮김
+    result.append(str(q.popleft()))
 
 print('<{}>'.format(', '.join(result)))
