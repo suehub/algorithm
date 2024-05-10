1a306351914076bd33e46548a6f7edc38564a364
이연수 <dldustn2040@gmail.com>
2024-05-10 18:17:48 +0900

    [Silver III] Title: 반전 요세푸스, Time: 444 ms, Memory: 245004 KB -BaekjoonHub

--- 백준/Silver/20301. 반전 요세푸스/반전 요세푸스.py
+++ 백준/Silver/20301. 반전 요세푸스/반전 요세푸스.py
@@ -1,17 +1,16 @@
 import sys
 from collections import deque
 
-result = []
-cnt = 0
 n, k, m = map(int, sys.stdin.readline().split())
-q = deque([i for i in range(1, n+1)])
+q = deque(range(1, n+1))
+cnt = 0
 
 while q:
-    if cnt != 0 and cnt % m == 0:
-        q.reverse()
-    q.rotate(-k+1)
-    result.append(q.popleft())
+    if cnt//m % 2 == 0:
+        for _ in range(k-1):
+            q.append(q.popleft())
+    else:
+        for _ in range(k):
+            q.appendleft(q.pop())
     cnt += 1
-
-for x in result:
-    print(x)
+    print(q.popleft())
