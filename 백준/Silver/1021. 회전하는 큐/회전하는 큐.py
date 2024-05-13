819142c61cbbcdc80fa6ef37d77e708335c9fb71
이연수 <dldustn2040@gmail.com>
2024-05-13 17:10:06 +0900

    [Silver III] Title: 회전하는 큐, Time: 68 ms, Memory: 34016 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
@@ -3,18 +3,19 @@ from collections import deque
 
 n, m = map(int, sys.stdin.readline().split())
 q = deque(range(1, n+1))
+
 idx_list = list(map(int, sys.stdin.readline().split()))
 cnt = 0
 
 for x in idx_list:
-    if x == q[0]:   # 뽑아내려는 원소가 첫 번째 위치했을 때
-        q.popleft()
-    elif q.index(x) < len(q) - q.index(x):  # 왼/오 회전 최솟값 비교
-        cnt += q.index(x)
-        q.rotate(-q.index(x))
+    idx = q.index(x)
+    if idx < len(q) - idx:  # 왼/오 회전 최솟값 비교
+        cnt += idx
+        q.rotate(-idx)
         q.popleft()
     else:
-        cnt += len(q) - q.index(x)
-        q.rotate(len(q)-q.index(x))
+        cnt += len(q) - idx
+        q.rotate(len(q)-idx)
         q.popleft()
+
 print(cnt)
